4f0e980e5f06dd18d5809ce446f02a1e873cfd28
andrea <a.gisolfi4@studenti.unisa.it>
2023-12-30 12:14:34 +0100

    implementate le classi Utente e Ospedale

--- MediCare/src/main/java/DataTier/MediCare/Ospedale/Ospedale.java
+++ MediCare/src/main/java/DataTier/MediCare/Ospedale/Ospedale.java
@@ -1,4 +1,40 @@
 package DataTier.MediCare.Ospedale;
 
 public class Ospedale {
+    private String email;
+    private String nome;
+    private String paese;
+    private String numero;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPaese() {
+        return paese;
+    }
+
+    public void setPaese(String paese) {
+        this.paese = paese;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
--- MediCare/src/main/java/DataTier/MediCare/Utente/Utente.java
+++ MediCare/src/main/java/DataTier/MediCare/Utente/Utente.java
@@ -1,4 +1,73 @@
 package DataTier.MediCare.Utente;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.GregorianCalendar;
+
 public class Utente {
+    private String email;
+    private String password;
+    private String nome;
+    private String cognome;
+    private GregorianCalendar ddn;
+    private String numero;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        try {
+            MessageDigest digest =
+                    MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(password.getBytes(StandardCharsets.UTF_8));
+            this.password = String.format("%040x", new
+                    BigInteger(1, digest.digest()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public GregorianCalendar getDdn() {
+        return ddn;
+    }
+
+    public void setDdn(GregorianCalendar ddn) {
+        this.ddn = ddn;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
