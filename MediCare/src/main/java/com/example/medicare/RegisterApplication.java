522fb90905a2ec3b7faa08f3bb5bfe3089b5e826
Gi0NZ <g.nigro32@studenti.unisa.it>
2023-12-30 18:57:28 +0100

    Inizio layout pagina registrazione

---
+++ MediCare/src/main/java/com/example/medicare/RegisterApplication.java
@@ -0,0 +1,23 @@
+package com.example.medicare;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RegisterApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("register-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 414, 896);
+        stage.setTitle("MediCare");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
