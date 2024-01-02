bb073a5e8e06d6618e0a4f93333cc5912c200514
andrea <a.gisolfi4@studenti.unisa.it>
2024-01-02 17:05:12 +0100

    aggiunto il controllo per verificare se l'email è già presente nel database

--- MediCare/src/main/java/com/example/medicare/HomePageApplication.java
+++ MediCare/src/main/java/com/example/medicare/HomePageApplication.java
@@ -11,9 +11,10 @@ public class HomePageApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("HomePage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 414, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 414, 869);
         stage.setTitle("MediCare");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
