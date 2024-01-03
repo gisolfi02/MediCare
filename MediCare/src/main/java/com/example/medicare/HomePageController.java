a8ad3f7e0147f8dbdb98e003ecadd7d6cacb72be
Antonio <a.merola29@studenti.unisa.it>
2024-01-03 15:50:24 +0100

    Aggiunto Css

--- MediCare/src/main/java/com/example/medicare/HomePageController.java
+++ MediCare/src/main/java/com/example/medicare/HomePageController.java
@@ -1,8 +1,20 @@
 package com.example.medicare;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+public class HomePageController{
+    @FXML
+    private Button homeButton;
 
+    @FXML
+    private Button profiloButton;
+    public void initialize() {
+        // Aggiungi i tooltip programmaticamente se necessario
+        Tooltip homeTooltip = new Tooltip("Clicca per tornare alla Home");
+        Tooltip.install(homeButton, homeTooltip);
 
-public class HomePageController {
-
+        Tooltip profiloTooltip = new Tooltip("Visualizza il tuo profilo");
+        Tooltip.install(profiloButton, profiloTooltip);
+    }
 }
